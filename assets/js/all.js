b37d51280b4c67a230bdd5db51a22494266c4fab
YuChih Hu <hijac.hu@gmail.com>
2020-06-21 00:15:41 +0800

    Update 2020-06-20T16:15:13.702Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,7 +1,9 @@
 "use strict";
 
-console.log('Hello!');
 $(document).ready(function () {
-  console.log('HexSchool Hello!');
+  $(".btn-hamburger").click(function (e) {
+    e.preventDefault();
+    $(".menu").toggleClass("active");
+  });
 });
 //# sourceMappingURL=all.js.map
